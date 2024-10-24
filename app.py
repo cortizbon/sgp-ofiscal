1bab2d254aa082ba60709880005c05a9e4a2e569
Carlos Ortiz <carlosortiz@192.168.0.3>
2024-10-24 16:42:29 -0500

    push with pop and constant values

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ cols = ['Educación',
        'Libre Destinación', 'Deporte', 'Cultura', 'Libre Inversión', 'Fonpet',
        'Alimentación Escolar', 'Ribereños', 'Resguardos Indígenas',
        'Fonpet Asignaciones Especiales', 'Primera Infancia', 'Total']
-df = pd.read_csv('datasets/datos_detalle.csv')
+
+cols_pc = [f"{i}_pc" for i in cols]
+cols_pc = [f"{i}_pop" for i in cols_pc]
+
+df = pd.read_csv('datasets/datos_detalle3.csv')
 years = df['Año'].unique()
 gdf = gpd.read_file('https://bucket-ofiscal.s3.us-east-1.amazonaws.com/MGN_ADM_MPIO_GRAFICO.shp')
 year = st.select_slider("Seleccione un año: ", years)
@@ -22,7 +26,7 @@ filtro = df[df['Año'] == year]
 
 merge = filtro.merge(gdf[['mpio_cdpmp', 'geometry']], left_on='CodigoDANEEntidad', right_on='mpio_cdpmp').reset_index(drop=True)
 
-col = st.selectbox("Seleccione una variable", cols)
+col = st.selectbox("Seleccione una variable", cols_pc_pop)
 
 
 fil = gpd.GeoDataFrame(merge)[[col, 'geometry']]
